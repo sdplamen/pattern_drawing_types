f09eb7680e502f583a01ace6a5ed9e704086520b
Plamen Stoilkov <sdplamen@gmail.com>
2024-07-29 16:23:12 +0300

    Update code.

--- pattern_drawing.py
+++ pattern_drawing.py
@@ -58,3 +58,21 @@ for a in range(n):
         else:
             print(' ', end='')
     print()
+
+def triangle(n):
+    for i in range(1, n):
+        for j in range(1, i + 1):
+            print(j, end=' ')
+        print()
+
+    for j in range(1, n + 1):
+        print(j, end=' ')
+    print()
+
+    for i in range(n - 1, 0, -1):
+        for j in range(1, i + 1):
+            print(j, end=' ')
+        print()
+    return ''
+
+print(triangle(int(input())))
